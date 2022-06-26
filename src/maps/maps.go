498a9af15b2668ef86cecefb3974fcd238fbbd87
hong <honggi@gmail.com>
2022-06-25 20:58:11 -0700

    get inputs from stdin to generate map

--- src/maps/maps.go
+++ src/maps/maps.go
@@ -1,10 +1,29 @@
 package maps
 
-/*
 import (
-	"fmt"
+	//"fmt"
+	"math/rand"
 )
-*/
+
+type Tile struct {
+	current int
+	total   int
+	speed   int
+	//TODO:type    string
+}
+
+//TODO: use enum/iota
+
+func randomTile() Tile {
+	energon := rand.Intn(10)
+	tile := Tile{
+		current: energon,
+		total:   energon,
+		speed:   rand.Intn(3),
+	}
+
+	return tile
+}
 
 type Map struct {
 	name   string
@@ -13,15 +32,27 @@ type Map struct {
 	tiles  [][]Tile
 }
 
-type Tile struct {
+/*
+func (m *Map) String() string {
+	//TODO: print tiles
+	return fmt.Sprintf("name: %s\nlength: %d\nwidth: %d", m.name, m.length, m.width)
 }
+*/
 
-func Generate(name string, length, width int) Map {
-	m := Map{
-		name: name,
-		length: length,
-		width: width,
+func Generate(name string, length, width int) *Map {
+	tiles := make([][]Tile, length)
+
+	for l := 0; l < length; l++ {
+		tiles[l] = make([]Tile, width)
+		for w := 0; w < width; w++ {
+			tiles[l][w] = randomTile()
+		}
 	}
 
-	return m
+	return &Map{
+		name:   name,
+		length: length,
+		width:  width,
+		tiles:  tiles,
+	}
 }
